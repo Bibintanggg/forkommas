2b50f7a3f19d7755c249888b03485d9b4ee7350b
Bibintanggg <bintangyudhaa8@gmail.com>
2026-04-02 12:48:27 +0700

    feat: add @tailwindcss/postcss dependency and update PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
+  plugins: {
+    '@tailwindcss/postcss': {}, // <-- ganti 'tailwindcss' biasa
+    autoprefixer: {},
+  },
 };
